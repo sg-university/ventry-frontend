9c1c3610fe04779066961abd0081e834f616568a
KevinSiek <kevinsiekwidjanarko@gmail.com>
2023-05-09 17:46:17 +0700

    Update request contract items

--- src/models/value_objects/contracts/requests/managements/items/create_one_request.ts
+++ src/models/value_objects/contracts/requests/managements/items/create_one_request.ts
@@ -1,5 +1,5 @@
 import Request from "@/models/value_objects/contracts/requests/request";
-import CreateBody from "@/models/value_objects/contracts/requests/managements/account_permission_maps/create_body";
+import CreateBody from "@/models/value_objects/contracts/requests/managements/items/create_body";
 
 class CreateOneRequest extends Request {
     body: CreateBody
