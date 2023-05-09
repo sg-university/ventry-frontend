24fbbf89adfb8d7ca6609e3a172d90141b279b86
KevinSiek <kevinsiekwidjanarko@gmail.com>
2023-05-09 17:47:42 +0700

    Update request contract transactions

--- src/models/value_objects/contracts/requests/managements/transactions/create_body.ts
+++ src/models/value_objects/contracts/requests/managements/transactions/create_body.ts
@@ -3,9 +3,9 @@ import ValueObject from "@/models/value_objects/value_object";
 class CreateBody extends ValueObject {
     accountId: string;
     sellPrice: number;
-    timestamp: Date;
+    timestamp: string;
 
-    constructor(accountId: string, sellPrice: number, timestamp: Date) {
+    constructor(accountId: string, sellPrice: number, timestamp: string) {
         super();
         this.accountId = accountId;
         this.sellPrice = sellPrice;
